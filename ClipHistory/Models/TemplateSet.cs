6ebe2c79de75ef0a3a4d3ab49be4f9daf904f746
hoss70237-afk <hoss70237@gmail.com>
2026-06-26 19:01:37 +0900

    Create TemplateSet.cs

---
+++ ClipHistory/Models/TemplateSet.cs
@@ -0,0 +1,10 @@
+// File: ClipHistory/Models/TemplateSet.cs
+namespace ClipHistory.Models
+{
+    /// <summary>定型文のグループ（セット）を表すモデル</summary>
+    public sealed class TemplateSet
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
