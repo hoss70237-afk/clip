7cfaadb680da57f9fdec71e82dd332b0bb7f3657
hoss70237-afk <hoss70237@gmail.com>
2026-06-26 10:10:27 +0900

    Create AssemblyInfo.cs for ClipHistory
    Added assembly information for the ClipHistory project.

---
+++ ClipHistory/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+// File: ClipHistory/Properties/AssemblyInfo.cs
+using System.Reflection;
+using System.Windows;
+
+[assembly: AssemblyTitle("ClipHistory")]
+[assembly: AssemblyDescription("軽量・高速な常駐型クリップボード履歴ユーティリティ")]
+[assembly: AssemblyProduct("ClipHistory")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly)]
